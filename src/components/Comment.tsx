9d848df8cf4e689785b3fb1d2d2c42ea58a670d0
Florian Aumeier <f.aumeier@me.com>
2020-03-27 16:20:54 +0100

    feat: display a single post with comments

---
+++ src/components/Comment.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import PostMetaInfo from './PostMetaInfo';
+import {Comment} from '../hackernews/api'
+
+
+export default function CommentComponent({comment}: CommentProps) {
+    const nestedComments = (comment.children || []).map(comment => {
+        return <CommentComponent key={comment.id} comment={comment} />;
+      });
+    return (
+        <React.Fragment>
+        <div className="comments" style={commentStyle(comment.depth)}>
+          <PostMetaInfo
+            by={comment.by}
+            time={comment.time}
+            id={comment.id}
+            descendants={comment.descendants}
+          />
+          <p dangerouslySetInnerHTML={{ __html: comment.text }} />
+        </div>
+        {nestedComments}
+      </React.Fragment>
+    )
+}
+
+type CommentProps = {
+    comment: Comment
+}
+
+const commentStyle = (depth: number) => {
+    const paddingLeft = 10 * depth;
+    return {
+      background: "rgba(128, 128, 128, 0.1411764705882353)",
+      padding: "10px",
+      margin: `10px 10px 10px ${paddingLeft}px`,
+      "border-radius": "5px"
+    };
+  };
---
+++ src/components/PostComponent.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import PostMetaInfo from "./PostMetaInfo";
+import CommentComponent from "./Comment";
+import Title from "./Title";
+import { fetchItem, fetchComments } from "../hackernews/api";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import {Comment, Story} from '../hackernews/api'
+
+
+function PostComponent(props: PostComponentProps) {
+  const { id } = queryString.parse(props.location.search);
+  const [post, setPost] = useState<Story>();
+  const [loadingPost, setLoadingPost] = useState(true);
+  const [comments, setComments] = useState<Comment[]>();
+  const [loadingComments, setLoadingComments] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!Array.isArray(id) && id) {
+      fetchItem(parseInt(id!))
+        .then(post => {
+          setPost(post);
+          setLoadingPost(false);
+          return fetchComments(post.kids || [], 0);
+        })
+        .then((comments: Comment[]) => {
+          setComments(comments);
+          setLoadingComments(false);
+        })
+        .catch(({ message }) => {
+          setError(message);
+          setLoadingPost(false);
+          setLoadingComments(false);
+        });
+    }
+  }, [id]);
+
+  if (error) {
+    return <h1>Error!</h1>;
+  }
+
+  if (post !== undefined) {
+    console.log("state", {
+      post,
+      loadingComments,
+      loadingPost,
+      comments
+    })
+    return (
+        <React.Fragment>
+          {loadingPost === true ? (
+            <h1>Loading</h1>
+          ) : (
+            <React.Fragment>
+              <h1 className="header">
+                <Title url={post.url} title={post.title} id={post.id} />
+              </h1>
+              <PostMetaInfo
+                by={post.by}
+                time={post.time}
+                id={post.id}
+                descendants={post.descendants}
+              />
+              <p dangerouslySetInnerHTML={{ __html: post.text }} />
+            </React.Fragment>
+          )}
+          {loadingComments === true ? (
+            loadingPost === false && <h1>Loading...</h1>
+          ) : (
+            <React.Fragment>
+              {comments && (comments.map((comment: Comment) => (
+                <CommentComponent key={comment.id} comment={comment} />
+              )))}
+            </React.Fragment>
+          )}
+        </React.Fragment>
+      );
+  }
+
+  return <h1>Nothing to show..</h1>
+}
+
+interface PostComponentProps
+  extends RouteComponentProps<{ location?: string }> {}
+
+export default withRouter(PostComponent);
