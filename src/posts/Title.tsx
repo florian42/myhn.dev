734ab40ec187c1733b19a404475dcb41e2d56a3d
Florian <f.aumeier@me.com>
2020-12-09 18:54:52 +0100

    feat: show domain name for links

--- src/posts/Title.tsx
+++ src/posts/Title.tsx
@@ -7,14 +7,25 @@ const Title: React.FC<{
   id: number;
 }> = ({ url, title, id }) => {
   return url ? (
-    <a className="link" href={url}>
-      {title}
-    </a>
+    <>
+      <a className="link" href={url}>
+        {title}
+      </a>
+      <span style={{ color: "hsl(201, 23%, 47%)" }}>
+        {" "}
+        {getDomainFromUrl(url)}
+      </span>
+    </>
   ) : (
     <Link className="link" to={`/post?id=${id}`}>
       {title}
     </Link>
   );
 };
+
+function getDomainFromUrl(url: string) {
+  const parsedUrl = new URL(url);
+  return parsedUrl.hostname.replace("www.", "");
+}
 
 export default Title;
