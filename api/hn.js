65a4dc16f8807afdd8a8de8c19fa4c52c99f8671
Florian <f.aumeier@me.com>
2020-12-12 14:05:25 +0100

    try out post event

--- api/hn.js
+++ api/hn.js
@@ -2,6 +2,6 @@ exports.handler = async function (event, context) {
   // your server-side functionality
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify({ message: event }),
   };
 };
